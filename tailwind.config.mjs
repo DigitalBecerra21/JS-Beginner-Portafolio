21b07316fe80dddd627789ed50a7aeffa976cede
DigitalBecerra <ingenierobecerrabarcelo@gmail.com>
2024-03-18 19:08:09 -0600

    feat: Initialized NavButtons section on index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,5 +16,17 @@ export default {
 			}
 		},
 	},
-	plugins: [],
+	plugins: [function ({ addUtilities }) {
+		const newUtilities = {
+		  '.MySlate-text': {
+			textAlign: 'center',
+			padding: '0.5rem',
+			fontSize: '1.875rem', // equivalente a text-3xl en Tailwind CSS
+			fontWeight: '700', // equivalente a font-bold en Tailwind CSS
+			color: 'rgb(203 213 225)', // equivalente a text-slate-300 en Tailwind CSS
+		  },
+		};
+  
+		addUtilities(newUtilities, ['responsive', 'hover', 'focus']);
+	  },],
 }
