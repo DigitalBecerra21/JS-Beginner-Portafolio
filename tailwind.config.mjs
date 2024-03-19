6a3d15746f2266531a3fe331eae965959a0cc783
DigitalBecerra <ingenierobecerrabarcelo@gmail.com>
2024-03-18 18:42:24 -0600

    feat: Updated MySlateBody component structure to input a HTML structure instead of a string

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,9 +5,14 @@ export default {
 	{
 		extend: 
 		{
-			colors:
+
+			fontFamily: 
 			{
-				
+				'sans': ['ui-sans-serif', 'system-ui'],
+				'serif': ['ui-serif', 'Georgia'],
+				'mono': ['ui-monospace', 'SFMono-Regular'],
+				'display': ['Oswald'],
+				'body': ['"Open Sans"'],
 			}
 		},
 	},
